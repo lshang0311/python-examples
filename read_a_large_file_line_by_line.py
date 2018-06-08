590d5491c3a5a6841e36f28ecd718c6df1f3dcd6
Lei Shang <lei.shang@hotmail.com>
2018-06-08 22:02:30 +1000

    Added more examples

--- read_a_large_file_line_by_line.py
+++ read_a_large_file_line_by_line.py
@@ -5,13 +5,13 @@ https://stackoverflow.com/questions/8009882/how-to-a-read-large-file-line-by-lin
 
 """
 
-path = r"../data"
+path = r"./data"
 path = os.path.abspath(path)
 file_name = 'list_of_fruits_1.csv'
 full_file_name = os.path.join(path, file_name)
 
-with open(full_file_name) as fh:
+with open(full_file_name) as f:
     cnt = 0
-    for line in fh:
+    for line in f:
         print('line {}: {}'.format(cnt, line.strip()))
         cnt += 1
